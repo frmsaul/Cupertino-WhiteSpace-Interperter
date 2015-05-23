ee4c7a2d08dba0383d77afd925e29da320774eee
frmsaul <frmsaul@gmail.com>
2015-05-23 12:36:08 -0700

    added a test platform

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include "virtual_machine.hpp"
 
+#include "../tests/test_stack_man.hpp"
+
 using namespace std;
 
-void test_stack(){
-  virtual_machine VM();
-  cout << "was able to build" << endl;
-}
 
 
 int main(){
   cout << "White Space Interperter Testing" << endl;
-  test_stack();
+  test_stack_man();
   return 0;
 }
---
+++ tests/test_stack_man.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <assert.h>
+
+#include "../src/instruction.hpp"
+#include "../src/virtual_machine.hpp"
+
+void test_stack_man(){
+  cout << "testing stack manipulations" << endl;
+}
